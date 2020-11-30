869f515df94dd0e67b07696a7824b32d9f7cbd10
75002d4e2357cef779d6b8f868342d8a <40080536+Unkn03nUser@users.noreply.github.com>
2020-11-30 19:20:44 +0900

    Bug fixed

--- 벽돌깨기/Sound.cpp
+++ 벽돌깨기/Sound.cpp
@@ -7,6 +7,8 @@ void Sound::up_pitch_play(short pitch, short playtime, short repeat) {
 	}
 }
 void Sound::down_pitch_play(short pitch, short playtime, short repeat) {
-	Beep(500 - pitch, playtime);
-	pitch -= pitch;
+	for (short i = 0; i < repeat; i++) {
+		Beep(500 - pitch, playtime);
+		pitch -= pitch;
+	}
 }
